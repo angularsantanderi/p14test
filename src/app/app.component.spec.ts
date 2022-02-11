0da4496f176a166d655f4e195c6ed1ee0f4b06b4
angularsantanderi <angularsantanderi@gmail.com>
2022-02-11 13:52:28 +0100

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('Componente raiz', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -14,22 +14,10 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('La aplicación es creada', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'p11formularios'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('p11formularios');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('p11formularios app is running!');
-  });
 });
--- src/app/facturas/crear-factura/crear-factura.component.spec.ts
+++ src/app/facturas/crear-factura/crear-factura.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CrearFacturaComponent } from './crear-factura.component';
 
-describe('CrearFacturaComponent', () => {
+describe('Componente crear factura', () => {
   let component: CrearFacturaComponent;
   let fixture: ComponentFixture<CrearFacturaComponent>;
 
@@ -19,7 +19,14 @@ describe('CrearFacturaComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('El componente es creado', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Campo nombre cliente obligatorio', () => {
+      const controlCampo = component.formFactura.get('nombreCliente');
+      controlCampo.setValue('');
+      expect(controlCampo.valid).toBeFalsy();
+  })
+
 });
